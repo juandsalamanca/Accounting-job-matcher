ededa2bd97f00657c663cba5d780fa5504cc6788
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-05-20 15:35:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import json
+import pandas as pd
 from src.extract_leads import extract_leads
+from src.get_emails_from_linkedin import get_emails_from_linkedin
+from src.google_linkedin_people import google_linkedin_people
+from src.jop_post_scraper import jop_post_scraper
 
 st.header("Accounting-Job-Matcher")
 
+st.subheader("Get extract lead list end get emails")
 st.text("Use the cookie editor chrome extension to export your sales navigator cookies as JSON")
 cookie_input = st.text_area("Paste here your Sales Navigator cookie", height=300)
 cookie_input = cookie_input.replace("true", "True").replace("false", "False").replace("null", "None")
@@ -16,6 +21,22 @@ max_results = st.number_input("Maximum results extracted from the list")
 extract = st.button("Extract leads")
 if extract:
   leads = extract_leads(cookies, list_url, max_results)
-  for 
+  leads["LinkedIN_URL"] = []
+  leads["Email"] = []
+  for i in range(len(leads["Name"])):
+    name = leads["Name"][i]
+    title = leads["Title"][i]
+    company = leads["Company"][i]
+    peronal_info_string = name + title + company
+    linkedin_url = google_linkedin_people(personal_info_string)
+    leads["LinkedIN_URL"].append(linkedin_url)
+    email = get_email_from_linkedin(linkedin_url)
+    leads["Email"].append(email)
+  df = pd.DataFrame(leads)
+
+st.subheader("Scrape job posts")
+job_title = st.text_area("Write down here the job title you want to use for scraping job posts")
+    
+                     
 
 
