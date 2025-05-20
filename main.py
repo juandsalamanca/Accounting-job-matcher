35d9e36a9b0da88ed9413651238dfa6e1effc546
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-05-20 15:21:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+from src.extract_leads import extract_leads
 
 st.header("Accounting-Job-Matcher")
 
@@ -8,3 +9,8 @@ cookie_input = st.text_area("Paste here your Sales Navigator cookie", height=300
 cookie_input = cookie_input.replace("true", "True").replace("false", "False").replace("null", "None")
 cookies = json.loads(cookie_input)
 list_url = st.text_area("Paste here your Sales Navigator Lead list URL", height=300)
+
+st.text("It is recommended to not scrape more than a few hundred leads a day and preferably spaced out during the day")
+max_results = st.number_input("Maximum results extracted from the list")
+
+leads = extract_leads(
