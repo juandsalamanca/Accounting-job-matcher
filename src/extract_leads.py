4c131bf344af115a158bf6e1216ba40aeb84fa57
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-05-20 15:18:46 -0500

    Update extract_leads.py

--- src/extract_leads.py
+++ src/extract_leads.py
@@ -2,13 +2,19 @@ from apify_client import ApifyClient
 import streamlit as st
 import json
 
-def extract_leads(cookies, list_url):
+def extract_leads(cookies, list_url, max_results):
 
   scraped_info = {"Name":[], "Title":[], "Company":[]}
   # Initialize the ApifyClient with your API token
   ApifyKey = st.secrets["apify_key"]
   client = ApifyClient(ApifyKey)
-  
+  run_input = {
+    "cookies": cookies,
+    "search": list_url,
+    "start_page": 1,
+    "max_page": 0,
+    "max_results": max_results
+}
   # Run the Actor and wait for it to finish
   run = client.actor("oxVIg0fuZ0uMcLoyT").call(run_input=run_input)
   
