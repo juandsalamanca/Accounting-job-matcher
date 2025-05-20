7fffe723ba49b78977cae2e526790ad62593be9b
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-05-20 15:03:34 -0500

    Create get_emails_from_linkedin.py

---
+++ src/get_emails_from_linkedin.py
@@ -0,0 +1,27 @@
+import requests
+
+def get_emails_from_linkedin(linkedinUrl):
+  url = "https://api.apollo.io/v1/people/match"
+
+  data = {
+      "first_name": firstName,
+      "last_name": lastName,
+      "organization_name": companyName,
+      "linkedin_url": linkedinUrl,
+      "reveal_personal_emails": True,
+      # "webhook_url": "https://your_webhook_site"
+  }
+
+  headers = {
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'application/json',
+      'X-Api-Key': '2TADtgGV3mbxRyhvNgYFrA'
+  }
+
+  response = requests.request("POST", url, headers=headers, json=data)
+
+
+  re=response.json()
+
+
+  return re["person"]["email"],re["person"]["email_status"]
