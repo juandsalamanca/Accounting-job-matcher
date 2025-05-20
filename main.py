fd1ca846e707c7b6a614ceefb553d1c476b005df
Juan Diego Salamanca <162521369+juandsalamanca@users.noreply.github.com>
2025-05-20 18:20:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ st.header("Accounting-Job-Matcher")
 
 st.subheader("Get extract lead list end get emails")
 st.text("Use the cookie editor chrome extension to export your sales navigator cookies as JSON")
-cookie_input = st.text_area("Paste here your Sales Navigator cookie", height=30)
+cookie_input = st.text_area("Paste here your Sales Navigator cookie", height=100)
 cookie_input = cookie_input.replace("true", "True").replace("false", "False").replace("null", "None")
 cookies = json.loads(cookie_input)
-list_url = st.text_area("Paste here your Sales Navigator Lead list URL", height=30)
+list_url = st.text_area("Paste here your Sales Navigator Lead list URL", height=100)
 
 st.text("It is recommended to not scrape more than a few hundred leads a day and preferably spaced out during the day")
 max_results = st.number_input("Maximum results extracted from the list")
